2a3c4e2382c875ba1a553e44d94c1e0698ac894f
Dagony <dagony@gmail.com>
2017-12-05 17:16:45 +0100

    Assignment Pipes (Reverse & Sort)

---
+++ pipes-assignment-start/src/app/sort.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'sort'
+})
+export class SortPipe implements PipeTransform {
+
+  transform(value: Array<any>, field: string): any {
+   value.sort((a,b) => {
+     if (a.name < b.name) {
+       return -1;
+     } else if (a.name > b.name) {
+       return 1;
+     } else {
+       return 0;
+     }
+   });
+   return value;
+  }
+
+}
